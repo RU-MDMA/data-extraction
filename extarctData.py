1fc401d0f92602d97f5048a07156c1beb4150412
Yuval nadam <Yuval.nadam@post.runi.ac.il>
2025-05-03 12:48:47 +0300

    creating extarctDate file v2 - only using df

--- extarctData.py
+++ extarctData.py
@@ -7,7 +7,7 @@ output_file_path = "combined_data.csv"
 target_subdirs = {"baseline", "recovery"}
 expected_meetings = [f"meet {i}" for i in range(1, 18)]
 
-all_data = []
+df = pd.DataFrame()
 
 #subject --> meeting --> base\ther\reco --> csv
 
@@ -19,17 +19,17 @@ for dirpath, _, filenames in os.walk(root_drive):
             if filename.lower().endswith(".csv"):
                 file_path = os.path.join(dirpath, filename)
                 try:
-                    df = pd.read_csv(file_path)
-                    all_data.append(df)
+                    #df = pd.read_csv(file_path)
+                    df.append(pd.read_csv(file_path))
 
                     print(f"Loaded: {file_path}")
                 except Exception as e:
                     print(f"Failed to read {file_path}: {e}")
 
 # Combine all DataFrames
-if all_data:
-    combined_df = pd.concat(all_data, ignore_index=True) #also converts into DataFrame 
-    combined_df.to_csv(output_file_path, index=False)
+if df:
+    df.to_csv(output_file_path, index=False)
+    print(df)
     print(f"\nCombined CSV saved to {output_file_path}")
 else:
     print("No CSV files found in the specified folders.")
